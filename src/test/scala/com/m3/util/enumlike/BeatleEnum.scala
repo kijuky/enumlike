c02224e29cb605a233e980980bc60dcfc0e73866
Chris Birchall <c-birchall@m3.com>
2014-12-01 19:10:46 +0900

    More tests

---
+++ src/test/scala/com/m3/util/enumlike/BeatleEnum.scala
@@ -0,0 +1,5 @@
+package com.m3.util.enumlike
+
+object BeatleEnum extends Enumeration {
+  val John, Paul, George, Ringo = Value
+}
--- src/test/scala/com/m3/util/enumlike/CustomBinderBuildersSpec.scala
+++ src/test/scala/com/m3/util/enumlike/CustomBinderBuildersSpec.scala
@@ -5,6 +5,22 @@ import org.scalatest.{ Matchers, FlatSpec }
 
 class CustomBinderBuildersSpec extends FlatSpec with Matchers with CustomBinderBuilders {
 
+  behavior of "#enumPathBindable"
+
+  val beatleEnumPathBindable = enumPathBindable(BeatleEnum)
+
+  it should "bind a known String" in {
+    beatleEnumPathBindable.bind("name", "Ringo") should be(Right(BeatleEnum.Ringo))
+  }
+
+  it should "fail to bind an unknown String" in {
+    beatleEnumPathBindable.bind("name", "Chris") should be('left)
+  }
+
+  it should "unbind" in {
+    beatleEnumPathBindable.unbind("name", BeatleEnum.Ringo) should be("Ringo")
+  }
+
   behavior of "#enumLikePathBindable"
 
   val beatlePathBindable = enumLikePathBindable[Beatle]
@@ -19,6 +35,26 @@ class CustomBinderBuildersSpec extends FlatSpec with Matchers with CustomBinderB
 
   it should "unbind" in {
     beatlePathBindable.unbind("name", Ringo) should be("Ringo")
+  }
+
+  behavior of "#enumQueryStringBindable"
+
+  val beatleEnumQueryStringBindable = enumQueryStringBindable(BeatleEnum)
+
+  it should "bind a known String" in {
+    beatleEnumQueryStringBindable.bind("name", Map("foo" -> Seq("bar"), "name" -> Seq("Ringo"))) should be(Some(Right(BeatleEnum.Ringo)))
+  }
+
+  it should "fail to bind an unknown String" in {
+    beatleEnumQueryStringBindable.bind("name", Map("foo" -> Seq("bar"), "name" -> Seq("Chris"))).get should be('left)
+  }
+
+  it should "not bind if param is not present" in {
+    beatleEnumQueryStringBindable.bind("name", Map("foo" -> Seq("bar"))) should be(None)
+  }
+
+  it should "unbind" in {
+    beatleEnumQueryStringBindable.unbind("name", BeatleEnum.Ringo) should be("name=Ringo")
   }
 
   behavior of "#enumLikeQueryStringBindable"
