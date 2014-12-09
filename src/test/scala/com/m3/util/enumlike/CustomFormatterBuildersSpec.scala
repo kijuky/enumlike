7f75cd3465e2c3ac242253d496638c3633e3d54b
Vincent PERICART <v-pericart@m3.com>
2014-12-09 16:39:14 +0900

    Added 1 test

--- src/test/scala/com/m3/util/enumlike/CustomFormatterBuildersSpec.scala
+++ src/test/scala/com/m3/util/enumlike/CustomFormatterBuildersSpec.scala
@@ -21,7 +21,31 @@ class CustomFormatterBuildersSpec extends FunSpec with Matchers with CustomForma
     val values = findValues.toIndexedSeq
   }
 
+  private object F extends Enumeration {
+    val A, B, C = Value
+    val D = Value("NOT D")
+  }
+
   describe("enumFormatter") {
+    val eFormatter = enumFormatter(F)
+    it("should unbind as toString") {
+      eFormatter.unbind("X", F.A) should be(Map("X" -> "A"))
+      eFormatter.unbind("X", F.D) should be(Map("X" -> "NOT D"))
+    }
+    it("should bind, failing with an error message (not translated since application is not running)") {
+      val bindFailures = Seq(
+        eFormatter.bind("X", Map()),
+        eFormatter.bind("X", Map("X" -> "D"))
+      )
+      bindFailures.foreach { bindResult =>
+        val fe = bindResult.left.get.head
+        fe.message should include("error")
+      }
+      eFormatter.bind("X", Map("X" -> "NOT D")).right.get should be(F.D)
+    }
+  }
+
+  describe("enumLikeFormatter") {
     import play.api.data.format.Formats.stringFormat
     val eFormatter = enumLikeFormatter[E]
     it("should unbind as toString") {
