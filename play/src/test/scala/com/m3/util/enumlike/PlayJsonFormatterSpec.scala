b035d9ec40f9e730353ae8e11e3c05cd26d3f9fa
Vincent PERICART <v-pericart@m3.com>
2015-04-24 17:15:26 +0900

    Added test

---
+++ play/src/test/scala/com/m3/util/enumlike/PlayJsonFormatterSpec.scala
@@ -0,0 +1,29 @@
+package com.m3.util.enumlike
+
+import org.scalatest.{ FlatSpec, Matchers }
+import play.api.libs.json.{ JsString, JsSuccess, Json }
+
+class PlayJsonFormatterSpec extends FlatSpec with Matchers with PlayJsonFormatter {
+
+  sealed trait Foo extends StringEnumLike
+  object Foo extends EnumCompanion[Foo] {
+
+    case object A extends Foo
+    case object B extends Foo
+    case object C extends Foo
+
+    val values = findValues
+
+  }
+
+  behavior of "JSON serialization"
+
+  it should "serialize to a JSON string" in {
+    Json.toJson(Foo.A) should be(JsString("A"))
+  }
+
+  it should "deserialize from a JSON string" in {
+    Json.fromJson[Foo](JsString("B")) should be(JsSuccess(Foo.B))
+  }
+
+}
